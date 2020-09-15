2f05644b1f912a0eefb5d7cbc15b9e703507e8d3
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2020-09-15 18:11:00 -0400

    Setup Complete
    Prefabs and Materials created, placeholder art and camera set

---
+++ Assets/Scripts/AppleTreeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTreeController : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject apple;
+    public float speed;
+    public float leftNright;
+    public float chance;
+    public float spawnTime;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftNright)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftNright)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+}
